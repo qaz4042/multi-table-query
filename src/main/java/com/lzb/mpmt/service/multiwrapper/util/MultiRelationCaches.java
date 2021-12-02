19fda474b1132cd89116b42d6a1a1956c7424100
lzubin <404286846@qq.com>
2021-12-02 18:23:54 +0800

    fix: caches优化,代码优化

--- src/main/java/com/lzb/mpmt/service/multiwrapper/util/MultiRelationCaches.java
+++ src/main/java/com/lzb/mpmt/service/multiwrapper/util/MultiRelationCaches.java
@@ -2,7 +2,6 @@ package com.lzb.mpmt.service.multiwrapper.util;
 
 import com.lzb.mpmt.service.multiwrapper.annotations.MultiTableField;
 import com.lzb.mpmt.service.multiwrapper.annotations.MultiTableId;
-import lombok.SneakyThrows;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -25,18 +24,15 @@ public class MultiRelationCaches {
      * 表的主键(对应的Field)
      */
     private static final Map<Class<?>, Field> TABLE_CLASS_ID_FIELD_MAP = new WeakHashMap<>(4096);
-
     /**
      * 将resultSet数据set到实体里,需要判断数据类型
      */
     private static final Map<Class<?>, Map<String, MultiTuple2<Field, Method>>> TABLE_CLASS_FIELD_SET_METHOD_MAP = new WeakHashMap<>(4096);
-
-
     /**
-     * 将子表数据,get,set到主表去
+     * 主表-子表数据,get,set到主表去
+     * todo class放到key中
      */
     private static final Map<String, MultiTuple2<Method, Method>> TABLE_WITH_TABLE_GET_SET_METHOD_MAP = new WeakHashMap<>(4096);
-
 
     /**
      * 表中每个属性和set方法
@@ -46,24 +42,21 @@ public class MultiRelationCaches {
     }
 
     /**
-     * 表中每个属性的set方法
+     * 表中所有属性名称
      */
-    public static Class<?> getFieldType(String fieldName, Class<?> tableClass) {
-        Map<String, MultiTuple2<Field, Method>> map = computeIfAbsentClassFieldMap(tableClass);
-        MultiTuple2<Field, Method> fieldNow = map.get(fieldName);
-        MultiUtil.assertNoNull(fieldNow,"找不到{0}对应的{1}属性",tableClass,fieldName);
-        return fieldNow.getT1().getType();
-    }
-
     public static List<String> getFieldNamesByClass(Class<?> tableClass) {
         return computeIfAbsentClassFieldMap(tableClass).values().stream().map(tuple2 -> MultiUtil.camelToUnderline(tuple2.getT2().getName())).collect(Collectors.toList());
     }
 
+    /**
+     * 获取主表下某个关联表对象(列表)的get/set方法
+     *
+     * @param relationCode 关联表对应relationCode
+     * @param tableClass   主表类
+     */
     public static MultiTuple2<Method, Method> getTableWithTable_getSetMethod(String relationCode, Class<?> tableClass) {
-        MultiTuple2<Method, Method> methods = TABLE_WITH_TABLE_GET_SET_METHOD_MAP.get(relationCode);
-//        String fieldNameUpperFirst = MultiUtil.firstToUpperCase(MultiUtil.underlineToCamel(relationCode));
-        String fieldNameUpperFirst = MultiUtil.firstToUpperCase(relationCode);
-        if (methods == null) {
+        return TABLE_WITH_TABLE_GET_SET_METHOD_MAP.computeIfAbsent(relationCode, (code) -> {
+            String fieldNameUpperFirst = MultiUtil.firstToUpperCase(code);
             //初始化map
             Method getMethod = MultiUtil.getAllMethods(tableClass).stream()
                     .filter(m -> unStaticUnFinal(m.getModifiers()))
@@ -77,30 +70,29 @@ public class MultiRelationCaches {
                     .filter(m -> m.getParameterTypes().length == 1 && !MultiUtil.isBasicDataType(m.getParameterTypes()[0]))
                     .findAny().orElse(null);
 
-            MultiUtil.assertNoNull(setMethod, "找不到{0}对应的get{1}", tableClass,fieldNameUpperFirst);
-            MultiUtil.assertNoNull(getMethod, "找不到{0}对应的get{1}", tableClass,fieldNameUpperFirst);
+            MultiUtil.assertNoNull(setMethod, "找不到{0}对应的get{1}", tableClass, fieldNameUpperFirst);
+            MultiUtil.assertNoNull(getMethod, "找不到{0}对应的get{1}", tableClass, fieldNameUpperFirst);
 
-            TABLE_WITH_TABLE_GET_SET_METHOD_MAP.put(relationCode, new MultiTuple2<>(getMethod, setMethod));
-        }
-        return TABLE_WITH_TABLE_GET_SET_METHOD_MAP.get(relationCode);
+            return new MultiTuple2<>(getMethod, setMethod);
+        });
     }
 
-
-    @SneakyThrows
+    /**
+     * 获取表的id属性
+     *
+     * @param tableClass 表对应类
+     */
     public static Field getTableIdField(Class<?> tableClass) {
-        Field idField = TABLE_CLASS_ID_FIELD_MAP.get(tableClass);
-        if (idField == null) {
-            List<Field> allFields = MultiUtil.getAllFields(tableClass);
-            idField = allFields.stream().filter(f -> null != f.getAnnotation(MultiTableId.class)).findAny().orElse(null);
+        return TABLE_CLASS_ID_FIELD_MAP.computeIfAbsent(tableClass, (clazz) -> {
+            List<Field> allFields = MultiUtil.getAllFields(clazz);
+            Field idField = allFields.stream().filter(f -> null != f.getAnnotation(MultiTableId.class)).findAny().orElse(null);
             if (idField == null) {
                 idField = allFields.stream().filter(f -> "id".equals(f.getName())).findAny().orElse(null);
             }
             MultiUtil.assertNoNull(idField, "找不到id字段(或者@MutilTableId对应属性){0}", tableClass);
-            TABLE_CLASS_ID_FIELD_MAP.put(tableClass, idField);
-        }
-        return idField;
+            return idField;
+        });
     }
-
 
     private static Map<String, MultiTuple2<Field, Method>> computeIfAbsentClassFieldMap(Class<?> tableClass) {
         return TABLE_CLASS_FIELD_SET_METHOD_MAP.computeIfAbsent(tableClass, (key) -> MultiUtil.getAllFields(tableClass).stream()
@@ -117,5 +109,4 @@ public class MultiRelationCaches {
     private static boolean unStaticUnFinal(int modifiers) {
         return !Modifier.isStatic(modifiers) && !Modifier.isFinal(modifiers);
     }
-
 }
